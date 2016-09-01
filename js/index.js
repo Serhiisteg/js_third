103d6efac64d2f5f10177cc6b49581b4ef4f8712
Serhii <klamozxc@gmail.com>
2016-09-01 14:59:42 +0000

    file

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(function() {
+   
+    // $('html').click(clock);
+     $('html').click(effect);
+    // setInterval(clock, 1000);
+// });
+// function clock() {
+//     $('body').html((new Date).toLocaleTimeString());
+// }
+function effect() {
+    $('h1').toggle(1000);
+}
+    
+// $(function() {
+//     $('body').html('<h1>Hello from jQuery!</h1>');
+});
